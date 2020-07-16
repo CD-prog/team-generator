f5904c081bbf9c1add6cf4128f9a49d40f5f31d6
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-07-16 18:19:47 -0500

    tests passing

--- Develop/app.js
+++ Develop/app.js
@@ -14,6 +14,52 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Role:",
+            name: "role",
+            choices: ["Intern", "Engineer", "Manager"]
+        },
+        {
+            type: "input",
+            message: "Name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "ID:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "email:",
+            name: "email"
+        },
+        
+    ])
+    .then(function (response) {
+
+        inquirer
+        .prompt ([ {
+            type: "input",
+            message: "School:",
+            name: "school"
+        },
+        {
+            type: "input",
+            message: "GitHub profile:",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "Office Number:",
+            name: "officeNumber",
+        },
+
+        ])
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,17 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Intern extends Employee {
+    constructor(name, id, email,school){
+        super(name, id, email)
+        this.school = school
+    }
+    getSchool(){
+        return this.school
+    }
+    getRole(){
+        return 'Intern'
+    }
+}
+module.exports = Intern;
+
+// const intern = new Intern('Michel',2,'michael@michael.com','northwestern')
+// intern.getSchool()
